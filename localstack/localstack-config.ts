2b469e8fb273d5e0bf52faf720df0abc7ee37b54
Elisandro Mello Correa <elisandro@ulbra.edu.br>
2025-06-02 23:12:08 -0300

    refactor: update variables structure validation

--- localstack/localstack-config.ts
+++ localstack/localstack-config.ts
@@ -1,12 +1,12 @@
 import { variables } from '../src/main/variables'
 
 export const checkIsEnabled = {
-  s3: variables.checkLocalStackS3,
-  sqs: variables.checkLocalStackSqs,
-  lambda: variables.checkLocalStackLambda,
-  apigateway: variables.checkLocalStackApiGateway,
-  dynamodb: variables.checkLocalStackDynamoDb,
-  cloudwatch: variables.checkLocalStackCloudWatch,
-  sns: variables.checkLocalStackSns,
-  kinesis: variables.checkLocalStackKinesis
+  s3: variables.checkLocalstack.s3,
+  sqs: variables.checkLocalstack.sqs,
+  lambda: variables.checkLocalstack.lambda,
+  apigateway: variables.checkLocalstack.apigateway,
+  dynamodb: variables.checkLocalstack.dynamodb,
+  cloudwatch: variables.checkLocalstack.cloudwatch,
+  sns: variables.checkLocalstack.sns,
+  kinesis: variables.checkLocalstack.kinesis
 }
